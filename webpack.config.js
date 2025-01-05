ae39b9984ea55dad7789856721ff817e2639e94c
Harshita Sharma <23053364@kiit.ac.in>
2025-01-05 22:13:46 +0530

    change

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
-
-  // Add fallbacks for missing Node.js modules
-  config.resolve.fallback = {
-    ...config.resolve.fallback,
-    vm: require.resolve('vm-browserify'),
-    stream: require.resolve('stream-browserify'),
-    crypto: require.resolve('crypto-browserify'),
-  };
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['@react-navigation']
+    }
+  }, argv);
 
   return config;
 };
