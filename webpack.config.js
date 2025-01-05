70b6e4efa87054c9a8b8d1cd910b343836e1b801
Harshita Sharma <23053364@kiit.ac.in>
2025-01-05 23:03:28 +0530

    chnage

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-module.exports = async function (env, argv) {
 
-  plugins: [
-    new BundleAnalyzerPlugin(),
-  ]
-  const config = await createExpoWebpackConfigAsync({
-    ...env,
-    babel: {
-      dangerouslyAddModulePathsToTranspile: ['@react-navigation']
-    }
-  }, argv);
+module.exports = async function (env, argv) {
+  // Create the default Expo webpack config
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {
+        dangerouslyAddModulePathsToTranspile: ['@react-navigation'],
+      },
+    },
+    argv
+  );
+
+  // Add the BundleAnalyzerPlugin only if `argv.mode` is defined and it's 'production'
+  if (argv && argv.mode === 'production') {
+    config.plugins.push(new BundleAnalyzerPlugin());
+  }
 
   return config;
 };
+
